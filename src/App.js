db44dc23e5e2af3d90f3f1c8569ec264263e2231
So Cool <127.0.0.1.pm@gmail.com>
2023-09-22 13:15:44 +0700

    add meals components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Fragment } from "react";
+
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
