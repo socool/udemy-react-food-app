dddfb6c622c4c0b54fe3380ce67bf02f8e6c18ac
So Cool <127.0.0.1.pm@gmail.com>
2023-09-22 13:41:00 +0700

    add individula MealItem components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,7 @@
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -26,14 +29,19 @@ const DUMMY_MEALS = [
   },
 ];
 const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
-      <ul>
-        {DUMMY_MEALS.map((meal) => (
-          <li>{meal.name}</li>
-        ))}
-      </ul>
-      <h1>Available Meals</h1>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
